3859904607b815ae2458fd4aed9500e8520a8b87
jjhangiani <47749517+jjhangiani@users.noreply.github.com>
2019-10-27 16:39:09 +0530

    Add files via upload

--- armstrong.php
+++ armstrong.php
@@ -4,11 +4,21 @@ $onum = $_POST["ip"];
 $num = $onum;
 $rem = 0;
 $sum = 0;
+$digit = 0;
+$j=2;
 
+while($num!=0)
+{
+	$digit +=1;
+	$num = $num/10;
+	$num = floor($num);
+	
+}
+$num=$onum;
 while($num !=0)
 {
 	$rem = $num%10;
-	$sum += $rem*$rem*$rem;
+	$sum += $rem**$digit;
 	$num /= 10;
 }
 
@@ -16,4 +26,5 @@ if($sum == $onum)
 	echo"THE NUMBER IS ARMSTRONG";
 else
 	echo"THE NUMBER IS NOT ARMSTRONG";
+
 ?>
